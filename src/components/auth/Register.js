bc31093453f8280adbcdc34204c6fad2fcd9f810
Home <home@Homes-MacBook.local>
2019-12-06 15:18:45 -0700

    Added react hooks to change state, put an onChange function in each of my fields and that goes to an onChange function which changes the state from all of what is in formData(spread operator) to the e.target name to the e.target value. Very cool.

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
-    return (
-        <div>
-            Register
-        </div>
-    )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    pasword: '',
+    password2: ''
+  }); // when using Hooks the first variable is our state and the second variable is the set state.
+  // What goes inside of useState is our default values.
 
-export default Register
+  const { name, email, password, password2 } = formData; // this is destructuring so we don't have to type formData in every time.
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value }); //this onChange function acts as our setState to change the formData each time there is a change in our form by taking the e.target.name (the name attribute in each field) and changing it to the value attribute}
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' action='create-profile.html'>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)} // we could call setFormData directly but we are going to call an OnChange function instead
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            value={password2}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
