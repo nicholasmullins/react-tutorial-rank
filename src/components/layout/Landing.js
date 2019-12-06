8a28c08d055d33c34df84366d8b42388e57864d1
Home <home@Homes-MacBook.local>
2019-12-06 13:46:29 -0700

    implementing the HTML templates for each component, building my navbar and landing page

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Tutorial Rank</h1>
+          <p className='lead'>
+            Find the best tutorials for every programming language. Sign in to
+            review and vote for the best video for learning each language.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
