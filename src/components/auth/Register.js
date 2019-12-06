13d2175de7fac41ee35f501f0b3d049a85e0218c
Home <home@Homes-MacBook.local>
2019-12-06 15:34:36 -0700

    Now able to submit all changes to the form and use useState to set the state, also put a passwords don't match safeguard in the onSubmit function for the register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -7,12 +8,21 @@ const Register = () => {
     pasword: '',
     password2: ''
   }); // when using Hooks the first variable is our state and the second variable is the set state.
-  // What goes inside of useState is our default values.
+  // What goes inside of useState is our default values. Also, by using hooks we don't have to create a state component and pass down anything! Brilliant.
 
   const { name, email, password, password2 } = formData; // this is destructuring so we don't have to type formData in every time.
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value }); //this onChange function acts as our setState to change the formData each time there is a change in our form by taking the e.target.name (the name attribute in each field) and changing it to the value attribute}
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
 
   return (
     <Fragment>
@@ -20,7 +30,7 @@ const Register = () => {
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' action='create-profile.html'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='text'
@@ -38,6 +48,7 @@ const Register = () => {
             name='email'
             value={email}
             onChange={e => onChange(e)}
+            required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -52,6 +63,7 @@ const Register = () => {
             minLength='6'
             value={password}
             onChange={e => onChange(e)}
+            required
           />
         </div>
         <div className='form-group'>
@@ -62,6 +74,7 @@ const Register = () => {
             minLength='6'
             value={password2}
             onChange={e => onChange(e)}
+            required
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
