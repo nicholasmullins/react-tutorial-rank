7c280cb94f210f8bb5f90aa3827b37dc534a5876
Home <home@Homes-MacBook.local>
2019-12-11 15:42:02 -0700

    made opening navbar design change

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
+        <Link to='/videos'>Videos</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
